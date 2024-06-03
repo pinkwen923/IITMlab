23e9922cb3d927fc841a08a1a8a6f305043d4064
Yi Wen Liao <pinkwen923@gmail.com>
2024-06-03 20:59:18 +0800

    update

--- src/content/labweb.js
+++ src/content/labweb.js
@@ -1,25 +1,23 @@
-import { Home, About, Lab, Teacher, Published, Patent, Thesis, PopularScience, Plan, Compete, Activity } from "./page";
+import { Home, About, Lab, Teacher, Patent, Thesis, PopularScience, Plan, Compete, Activity } from "./page";
 import { Error404 } from './page/404';
-import { Routes, Route, Navigate, } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function LabWeb() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} ></Route>
-      <Route path="" element={<Home />} ></Route>
-      <Route path="/IITMlab" element={<Home />} ></Route>
-      <Route path="/iitmlab" element={<Home />} ></Route>
-      <Route path="/Lab" element={<Lab />}></Route>
-      <Route path="/Teacher" element={<Teacher />}></Route>
-      <Route path="/patent" element={<Patent />}></Route>
-      <Route path="/thesis" element={<Thesis />}></Route>
-      <Route path="/popularScience" element={<PopularScience />}></Route>
-      <Route path="/plan" element={<Plan />}></Route>
-      <Route path="/compete" element={<Compete />}></Route>
-      <Route path="/activity" element={<Activity />}></Route>
-     
-      <Route path="*" element={<Error404 />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/iitmlab" element={<Navigate to="/" />} />
+      <Route path="/lab" element={<Lab />} />
+      <Route path="/teacher" element={<Teacher />} />
+      <Route path="/patent" element={<Patent />} />
+      <Route path="/thesis" element={<Thesis />} />
+      <Route path="/popularscience" element={<PopularScience />} />
+      <Route path="/plan" element={<Plan />} />
+      <Route path="/compete" element={<Compete />} />
+      <Route path="/activity" element={<Activity />} />
+      <Route path="*" element={<Error404 />} />
     </Routes>
   )
 }
+
 export default LabWeb;
