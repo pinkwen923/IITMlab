92219a59a7fd713d926ae094449d546d40fc5109
Yi Wen Liao <pinkwen923@gmail.com>
2024-06-03 21:15:08 +0800

    update

--- src/content/labweb.js
+++ src/content/labweb.js
@@ -1,22 +1,24 @@
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Home, About, Lab, Teacher, Patent, Thesis, PopularScience, Plan, Compete, Activity } from "./page";
 import { Error404 } from './page/404';
-import { Routes, Route, Navigate } from "react-router-dom";
 
 function LabWeb() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/iitmlab" element={<Navigate to="/" />} />
-      <Route path="/lab" element={<Lab />} />
-      <Route path="/teacher" element={<Teacher />} />
-      <Route path="/patent" element={<Patent />} />
-      <Route path="/thesis" element={<Thesis />} />
-      <Route path="/popularscience" element={<PopularScience />} />
-      <Route path="/plan" element={<Plan />} />
-      <Route path="/compete" element={<Compete />} />
-      <Route path="/activity" element={<Activity />} />
-      <Route path="*" element={<Error404 />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/iitmlab" element={<Navigate to="/" />} />
+        <Route path="/lab" element={<Lab />} />
+        <Route path="/teacher" element={<Teacher />} />
+        <Route path="/patent" element={<Patent />} />
+        <Route path="/thesis" element={<Thesis />} />
+        <Route path="/popularscience" element={<PopularScience />} />
+        <Route path="/plan" element={<Plan />} />
+        <Route path="/compete" element={<Compete />} />
+        <Route path="/activity" element={<Activity />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
   )
 }
 
